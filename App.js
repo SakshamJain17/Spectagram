1cfa328a144fcdefbed443c99ca2d7f7e616c2b4
SakshamJain17 <71916941+SakshamJain17@users.noreply.github.com>
2022-07-23 13:32:14 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerNavigator from './navigation/DrawerNavigation';
+import BottomTabNavigator from './navigation/TabNavigator';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>a</Text>
       <StatusBar style="auto" />
     </View>
   );
